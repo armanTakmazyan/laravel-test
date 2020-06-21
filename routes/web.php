8203914ff8dcd5cea1b9275316832f02e272ddc3
Arman Takmazyan <armanworkmail01@gmail.com>
2020-06-21 17:02:56 +0400

    added authentication scaffolding and removed ability to register

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('register', function () {
+	return redirect('/');
+})->name('register');
+
+Route::get('/home', 'HomeController@index')->name('home');
