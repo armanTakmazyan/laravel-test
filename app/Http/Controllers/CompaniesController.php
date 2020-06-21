368a55f6f3afba7b59ac3e5ccf75d2f3678c4a2e
Arman Takmazyan <armanworkmail01@gmail.com>
2020-06-22 00:10:06 +0400

    Added folders for companies and employees views. app.blade.php and UpdateCompanyRequest.php files also was changed slightly.

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateCompanyRequest;
+use App\Http\Requests\StoreCompanyRequest;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Company;
 
 class CompaniesController extends Controller
 {
-    //
+    /**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+		$companies = Company::orderBy('created_at', 'DESC')->paginate(10);
+		return view('companies.index', compact('companies'));
+	}
 }
