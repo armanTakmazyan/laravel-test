6a488d70ee534c0c1e36b0423ac3847420bf6199
HovoKhachibabyan <23hovo32@gmail.com>
2020-06-22 11:44:05 +0400

    Removing Registration link from the login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,12 +43,13 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
+                            
+                           @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                   <!-- <a class="nav-link" href="{{ route('register') }}">{{ __('Regisster') }}</a> -->
                                 </li>
                             @endif
-                        @else
+                        @else 
                             <li class="nav-item">
 								<a class="nav-link" href="{{ route('companies.index') }}"> Companies </a>
 							</li>
